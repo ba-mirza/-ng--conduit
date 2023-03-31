1fae880c7bc63b4b59f0b8dd580fd4a392406d42
almas_1610 <a.bissenaliyev@stepsai.com>
2023-03-31 10:01:14 +0600

    [updated] -> added interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import {AppComponent} from './app.component';
 import {AuthModule} from './auth/auth.module';
 import {StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {HttpClientModule} from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {EffectsModule} from '@ngrx/effects';
 import {TopBarModule} from './shared/modules/topBar/topBar.module';
+import {PersistanceService} from './shared/services/persistance.service';
+import {Authinterceptor} from './shared/services/authinterceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,7 +24,14 @@ import {TopBarModule} from './shared/modules/topBar/topBar.module';
     EffectsModule.forRoot([]),
     TopBarModule,
   ],
-  providers: [],
+  providers: [
+    PersistanceService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: Authinterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
